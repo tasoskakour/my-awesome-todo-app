29d92734be38570ed327f1b7b19b55431d50689e
tasos <tasoskakour@gmail.com>
2019-02-13 20:55:04 +0200

    Bootstrapped folders structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MainScreen } from './screens'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <MainScreen />
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/screens/MainScreen/MainScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class MainScreen extends React.Component {
+    render() {
+        return <div>This is the main screen</div>
+    }
+}
